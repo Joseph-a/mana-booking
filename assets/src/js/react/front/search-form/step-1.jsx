6ab742a59bcdecbdc2ea3070eaa1a7dcf6a9de7d
Yoosef Ahkami <yoosef.ahkami@nentgroup.com>
2020-12-30 10:22:08 +0100

    search form style changes

--- assets/src/js/react/front/search-form/step-1.jsx
+++ assets/src/js/react/front/search-form/step-1.jsx
@@ -53,6 +53,26 @@ const Step1 = (props) => {
         <Fragment>
             <div className="top-sec">
                 <div className="date-box">
+                    <div className="input-box">
+                        <label>{__('Check In', 'mana-booking')}:</label>
+                        <input
+                            type="text"
+                            placeholder={__('Check In', 'mana-booking')}
+                            value={startEndDatesField.startDate ? moment(startEndDatesField.startDate).format('YYYY-MM-DD') : ''}
+                            onClick={() => setFocusedInput('startDate')}
+                            readOnly
+                        />
+                    </div>
+                    <div className="input-box">
+                        <label>{__('Check Out', 'mana-booking')}:</label>
+                        <input
+                            type="text"
+                            placeholder={__('Check Out', 'mana-booking')}
+                            value={startEndDatesField.endDate ? moment(startEndDatesField.endDate).format('YYYY-MM-DD') : ''}
+                            onClick={() => setFocusedInput('endDate')}
+                            readOnly
+                        />
+                    </div>
                     <DateRangePicker
                         startDateId={`startDate_${randId}`}
                         endDateId={`endDate_${randId}`}
@@ -68,6 +88,7 @@ const Step1 = (props) => {
                     />
                 </div>
                 <div className="rooms">
+                    <label>{__('Rooms', 'mana-booking')}:</label>
                     <select
                         value={selectedRoomsField.length}
                         placeholder={__('Rooms', 'mana-booking')}
@@ -132,10 +153,11 @@ const Step1 = (props) => {
                                 </select>
                             </div>
                             <div className="room">
-                                <label>{__('Room', 'mana-booking')}:</label>
+                                {/* <label>{__('Room', 'mana-booking')}:</label> */}
+                                <label></label>
                                 <button
                                     onClick={() => selectRoom(startEndDatesField.startDate.format('YYYY-MM-DD'), startEndDatesField.endDate.format('YYYY-MM-DD'), item.adult, item.child)}
-                                >{__('Select Room', 'mana-booking')}</button>
+                                >{__('Check Availability', 'mana-booking')}</button>
                             </div>
                         </div>
                     ))
