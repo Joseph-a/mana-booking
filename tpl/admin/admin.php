f3818dd5e3e03a4ea2ae4dbe5ec9dd13b1c37995
Yoosef Ahkami <yoosef.ahkami@nentgroup.com>
2021-03-14 11:21:44 +0100

    fixing save setting issue

--- tpl/admin/admin.php
+++ tpl/admin/admin.php
@@ -8,7 +8,7 @@ $options_str = wp_json_encode($options);
 			<h1><?php esc_html_e('Mana Booking Settings', 'mana-booking') ?></h1>
 			<form action="options.php" method="post" id="mana-options-setting-form">
 				<?php settings_fields('mana-booking-setting'); ?>
-				<input type="hidden" name="mana-booking-setting[main_setting]" id="mana_booking_main_setting" value="<?php echo !empty($options['main_setting']) ? esc_attr($options['main_setting']) : '0' ?>" />
+				<input type="hidden" name="mana-booking-setting[main_setting]" id="mana_booking_main_setting" value="<?php echo !empty($options) ? esc_attr($options_str) : '0' ?>" />
 				<?php submit_button(); ?>
 			</form>
 		</div>
